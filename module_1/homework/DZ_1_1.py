8151de375d11b133550a29a2c37f6d8894edfb99
denisyakovlev05111985-cloud <denisyakovlev05111985@gmail.com>
2025-12-23 13:11:53 +0300

    feat: Домашнее задание

---
+++ module_1/homework/DZ_1_1.py
@@ -0,0 +1,6 @@
+a = int(input("Введите первое число: "))  
+b = int(input("Введите второе число: "))  
+c = int(input("Введите третье число: "))  
+sum = a + b + c
+multipliers = a * b * c
+print(f'Сумма = {sum} Произведение = {multipliers}')  
