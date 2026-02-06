b18824abce8d98ecd4923961a7889a089634bdae
denisyakovlev05111985-cloud <denisyakovlev05111985@gmail.com>
2026-02-06 16:59:35 +0300

    feat: закрепление изученного

--- module_5/classwork/part_1/task_4.py
+++ module_5/classwork/part_1/task_4.py
@@ -1,42 +1,54 @@
-# logs= [
-#     ('ivan', 'day', 8),
-#     ('ivan', 'night', 4),
-#     ('olga','day', 6),
-#     ('petr', 'night', 7),
-#     ('anna', 'day', 4),
-#     ('anna', 'day', 3),
-#     ('anna', 'night', 3),
-# ]
-# new_logs= {}
+logs= [
+    ('ivan', 'day', 11),
+    ('ivan', 'night', 1),
+    ('olga','day', 6),
+    ('petr', 'night', 1),
+    ('anna', 'day', 1),
+    ('anna', 'day', 3),
+    ('anna', 'night', 3),
+]
 
-# for el in logs:
-#     name= el[0]
-#     shift= el[1]
-#     if name not in new_logs:
-#         new_logs[name]= set()
-    
-#     new_logs[name].add(shift)
+# нати сотр которые работали в разных сменах
+# найти смены где суммарно меньше 8 часов
+# найти сотрудников у кого больше или равно 12 часов
 
-# for el2 in new_logs:
-#     value = new_logs[el2]
-#     if len(value)>1:
-#         print(el2)
+employee_shift= {}
+shift_hours= {}
+employee_hours= {}
 
-# new_logs_2={}
+for name, shift, hours in logs:
+    if name not in employee_shift:
+        employee_shift[name]= set()
+    employee_shift[name].add(shift)
 
-# for el in logs:
-#     shift= el[1]
-#     vremya= el[2]
-#     if shift not in new_logs_2:
-#         new_logs_2[shift]= 0
-    
-#     new_logs_2[shift] += vremya
+    if shift not in shift_hours:
+        shift_hours[shift] = 0
+    shift_hours[shift] += hours
 
-# for x in new_logs_2:
-#     sum_vremya= x[1]
-#     if sum_vremya <= 8:
-#         print()
-# print(new_logs_2)
-# # нати сотр которые работали в разных сменах
-# # найти смены где суммарно меньше 8 часов
-# # найти сотрудников у кого больше или равно 12 часов
+    if name not in employee_hours:
+        employee_hours[name] = 0
+    employee_hours[name] += hours
+
+print(employee_shift)
+print(shift_hours) 
+print(employee_hours)  
+
+multiple_shift= []
+for employee in employee_shift:
+    if len(employee_shift[employee]) == 2:
+        multiple_shift.append(employee)
+
+shifts_less= []
+for shift in shift_hours:
+    if shift_hours[shift] < 8:
+        shifts_less.append(shift)
+
+employees= []
+for employee in employee_hours:
+    if employee_hours[employee] >=12:
+        employees.append(employee)
+
+print(multiple_shift)
+print(shifts_less)
+print(employees)
+
